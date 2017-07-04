046ba34c7d65766a0e292635fdf8d7dc9fdaf458
Mark Lopez <m@silvenga.com>
2017-07-04 00:15:06 -0500

    Added sample project.

---
+++ src/Yocto.Contrib.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yocto.Contrib.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
