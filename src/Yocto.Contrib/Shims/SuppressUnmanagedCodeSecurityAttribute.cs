f5b81a9c3d62afab9dd8395becdfbc1b19332861
Mark Lopez <m@silvenga.com>
2017-07-04 00:14:53 -0500

    Updated pinvokes to allow dotnet to handle DllImport's.

--- src/Yocto.Contrib/Shims/SuppressUnmanagedCodeSecurityAttribute.cs
+++ src/Yocto.Contrib/Shims/SuppressUnmanagedCodeSecurityAttribute.cs
@@ -1,6 +1,7 @@
 ﻿// ReSharper disable once CheckNamespace
 namespace System.Security
 {
+    // TODO - Not strictly required and not support under .Net Standard.
     public class SuppressUnmanagedCodeSecurityAttribute : Attribute
     {
     }
